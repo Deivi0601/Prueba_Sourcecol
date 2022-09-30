8ef427f3f7f3c5d73a5a8fdbe261c09a199e17cb
jdmunoz <jdmunoz@garantiascomunitarias.com>
2022-09-30 10:56:40 -0500

    Add project files.

---
+++ Prueba_Sourcecol/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace SolveTestSourcecol
+{
+    public class Test
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Seleccionar opción a ejecutar");
+            Console.WriteLine("1. Cortar cadena de texto");
+
+            int option = int.Parse(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    {
+                        processCutTextString();
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("La opción seleccionada no es valida");
+                        break;
+                    }
+            }
+
+        }
+
+        /** 
+         *  Escribir cadena de texto y longitud máxima de carácteres a mostrar
+        */
+        public static void processCutTextString()
+        {
+            Console.WriteLine("Inicia primer punto");
+
+            Console.WriteLine("Escribir texto");
+            string text = Console.ReadLine();
+            Console.WriteLine("Escribir longitud aáxima");
+            int maxLength = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Resultado ---> " + cutTextString(text, maxLength));
+            
+        }
+
+        static string cutTextString(string text, int maxLength) => text.Substring(0, maxLength > text.Length ? text.Length : maxLength) + (text.Length > maxLength ? "..." : "");
+
+    }
+}
