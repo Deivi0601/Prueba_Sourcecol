ea144c9615de9b311c3250b95e656e9a12598db4
Jesus D. Muñoz G <jdmunoz@garantiascomunitarias.com>
2022-09-30 11:11:22 -0500

    Subir solución segundo punto

--- Prueba_Sourcecol/Program.cs
+++ Prueba_Sourcecol/Program.cs
@@ -9,6 +9,7 @@ namespace SolveTestSourcecol
 
             Console.WriteLine("Seleccionar opción a ejecutar");
             Console.WriteLine("1. Cortar cadena de texto");
+            Console.WriteLine("2. Validar valor en serie Fibonacci");
 
             int option = int.Parse(Console.ReadLine());
             switch (option)
@@ -16,6 +17,11 @@ namespace SolveTestSourcecol
                 case 1:
                     {
                         processCutTextString();
+                        break;
+                    }
+                case 2:
+                    {
+                        processValidateFibonacciSerie();
                         break;
                     }
                 default:
@@ -28,8 +34,8 @@ namespace SolveTestSourcecol
         }
 
         /** 
-         *  Escribir cadena de texto y longitud máxima de carácteres a mostrar
-        */
+         *  Ejecutar recorte de cadena de texto
+         */
         public static void processCutTextString()
         {
             Console.WriteLine("Inicia primer punto");
@@ -43,7 +49,63 @@ namespace SolveTestSourcecol
             
         }
 
+        /**
+         * Cortar cadena de texto según longitud máxima de carácteres
+         */
         static string cutTextString(string text, int maxLength) => text.Substring(0, maxLength > text.Length ? text.Length : maxLength) + (text.Length > maxLength ? "..." : "");
+
+        /**
+         * Ejecutar validación de valor en serie Fibonacci
+         */
+        public static void processValidateFibonacciSerie()
+        {
+            Console.WriteLine("Inicia segundo punto");
+            Console.WriteLine("Escribir un valor de la serie de Fibonacci");
+            int valueFibonacci = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Resultado ---> " + validateFibonacciSerie(valueFibonacci));
+        }
+
+        /** 
+         *  Validar si el valor enviado existe en la serie Fibonacci
+         */
+        static string validateFibonacciSerie(int valueFibonacci)
+        {
+            // Mensaje de respuesta a retornar
+            string messageResponse = "";
+
+            int value1 = 0;
+            int value2 = 1;
+
+            Console.WriteLine("Serie");
+
+            for (int i = 0; i < valueFibonacci; i++)
+            {
+                int valueTemp = value1;
+
+                value1 = value2;
+
+                value2 = valueTemp + value1;
+
+                Console.Write(value1 + " ");
+
+                // Si el valor de la secuencia ya supero el valor enviado es porque no coincide
+                if (value1 > valueFibonacci)
+                {
+                    messageResponse = $"El valor {valueFibonacci} no coincide con la serie de Fibonacci";
+                    break;
+                }
+                else if (value1 == valueFibonacci)
+                {
+                    messageResponse = $"El valor {valueFibonacci} coincide con la serie de Fibonacci";
+                    break;
+                }
+
+            }
+
+            return messageResponse;
+        }
+
 
     }
 }
